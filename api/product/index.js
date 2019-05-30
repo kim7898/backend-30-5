3f517333c95e1ecbaa5cde089f8684153fd2091e
oanh <vuthikimoanh7898@gmail.com>
2019-05-30 21:00:43 +0700

    30/05

--- api/product/index.js
+++ api/product/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: "/uploads"});
+const upload = multer({dest: "/app/uploads"});
 const { createProduct, getAllProduct, getOneProduct, deleteProduct } = require('./controller');
 
 router.post("/",upload.single("image"), (req, res) => {
